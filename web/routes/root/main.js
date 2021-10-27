4e981b78ca3c7152c7a4b5c392facfc0ba0bc306
NisamVikki <viktorblazaric2@gmail.com>
2021-10-28 00:51:41 +0200

    Update main.js

--- web/routes/root/main.js
+++ web/routes/root/main.js
@@ -7,6 +7,14 @@ router.use("/admin-apply", require('./admin-apply'))
 
 router.use("/", function(req, res) {
     res.send(`
+        <head>
+            <title> </title>
+            <meta content="Wolfie Dashboard" property="og:title" />
+            <meta content="Zdravo! Ovde imate sve sta mozete da radite na wolfie sajtu, dokumentaciju isto imate." property="og:description" />
+            <meta content="https://discord-wolfie.herokuapp.com/main" property="og:url" />
+            <meta content="" property="og:image" />
+            <meta content="#087CF0" data-react-helmet="true" name="theme-color" />
+        </head>
         <body style="background-color:#3c3c3c;">
         <center>
             <h1 style="color:white;font-family: Arial, Helvetica, sans-serif;"> ${(req.user == undefined) ? "Dobrodosao!" : `Dobrodosao ${req.user.discordTag}`} </h1>
