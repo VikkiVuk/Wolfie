c2e764ce315ca8b09cdf7d638583f0e128a5fc10
NisamVikki <viktorblazaric2@gmail.com>
2022-01-30 02:31:27 +0100

    bghh

--- bot/events/message.js
+++ bot/events/message.js
@@ -35,10 +35,6 @@ module.exports = {
             }
         }
 
-        if (message.channel.id === '878606227595227164' || message.channel.id === '878606227595227165' || message.channel.id === '878606227800719440' || message.channel.id === '878606227800719441') {
-            message.delete()
-        }
-
         const englishInsults = ["nigga", "nigg", "nigeria", "negus", "nickgurr", "nword", "tosser", "wanker", "slag", "cheeseeatingsurrendermonkeys", "losttheplot", "daftcow", "arsehole", "barmy", "chav", "dodgy", "git", "gormless", "manky", "minger", "muppet", "naff", "nutter", "pikey", "pillock", "plonker", "prat", "scrubber", "trollop", "uphillgardener", "knobhead", "pissoff", "bellend", "lazysod", "skiver", "knob", "wazzock", "ninny", "berk", "airyfairy", "anklebiters", "arselicker", "arsemonger", "chuffer", "daftasabush", "deadfromtheneckup", "gannet", "gonetothedogs", "ligger", "likeadogwithtwodicks", "madasabagofferrets", "maggot", "mingebag", "notbattingonafullwicket", "plugugly"]
         const englishNsfwStuff = ["fuck", "hentai", "fck", "fucku", "vagina", "daddy", "cunt", "pussy", "penis", "dick", "cock", "boob", "tit", "ass", "bunda", "anus", "cum", "nsfw", "porn", "sperm", "shaft", "jizz", "sex"]
         const serbianInsults = ["suka","crnja", "cigan", "crn", "jebi", "nahasasmisetarzancica", "liznesmepodmisku", "rimtutitukiđumebaokao", "sisutiskrbavu", "celavatikeva", "smrditiizustakokonjuizdupeta", "pickatiseogadilakuractiseosladio", "pucaletijebokevu", "jebemlitisirairakiju", "seremtisepodsitnozito", "posrcesmiđanicunaslamcicu", "naprskamtiseprekopudera", "jedigovnasitpapijvode", "katizveknemsamarnigovnatinecebuduslatka", "shramtebilo"]
@@ -48,10 +44,10 @@ module.exports = {
             if (!message.member.roles.cache.has(nofilterrole)) {
                 if (englishInsults.some(s => loweredMsg.includes(s)) || englishNsfwStuff.some(s => loweredMsg.includes(s)) || serbianInsults.some(s => loweredMsg.includes(s)) || serbianNsfwStuff.some(s => loweredMsg.includes(s))) {
                     if (message.author.id === client.user.id || message.member.roles.cache.has('873680682096947210')) return;
-                    message.delete()
+                    /*message.delete()
                     message.author.send("Hey, dont be rude!").catch(() => {
                         message.channel.send(`${message.author} dont be rude to people, people have feelings yk`)
-                    })
+                    })*/
                 }
             }
         } else {
