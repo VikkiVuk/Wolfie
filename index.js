7bec3e0e0f70a608fd8f7eb49f8c5c83c8dcfd1e
NisamVikki <viktorblazaric2@gmail.com>
2022-02-15 15:10:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(session({secret: "secret", resave: false, saveUninitialized: false, cook
 app.use(passport.initialize())
 app.use(passport.session())
 
+app.use("/sitemap", (req, res) => {req.download(require("./web/sitemap.txt"))})
 app.use("/api", require("./web/routes/api/main.js"))
 app.use("/", require("./web/routes/root/main.js"))
 
