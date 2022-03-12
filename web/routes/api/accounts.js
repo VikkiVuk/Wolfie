1adc6cd4ff93c97b4cee95930b362aeb9804ad7f
NisamVikki <viktorblazaric2@gmail.com>
2022-03-12 17:59:35 +0100

    AAAAAA

--- web/routes/api/accounts.js
+++ web/routes/api/accounts.js
@@ -1,14 +1,25 @@
 const router = require('express').Router()
 const backend = require('../../../bot/utility/backend')
 const passport = require("passport")
+const got = require('got')
+let api = "https://accounts.vikkivuk.xyz/user/"
+
 router.get("/link/redirect", passport.authenticate('discord'), (req, res, next) => {
     res.redirect("https://accounts.vikkivuk.xyz/authorize?callback=https://wolfie.pro/api/accounts/link")
 })
-router.get("/link", async(req,res) => {
+router.get("/link", async(req,res, next) => {
     let user = await req.user
-    console.log(user)
     if (user) {
-        res.send("wait")
+        let params = await req.query
+        if (params.id) {
+            got.post(api + "get/" + params.id).then(async (response) => {
+                if (response.body) {
+                    console.log(response.body)
+                }
+            })
+        } else {
+            res.redirect("https://accounts.vikkivuk.xyz/authorize?callback=https://wolfie.pro/api/accounts/link")
+        }
     } else {
         passport.authenticate('discord')(req, res, next)
     }
