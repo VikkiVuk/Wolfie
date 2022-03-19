32db9f894c356c425a4de8a0544b09168fefe45a
NisamVikki <viktorblazaric2@gmail.com>
2022-03-19 20:25:05 +0100

    Bot Update

--- bot/utility/schemas/user-schema.js
+++ bot/utility/schemas/user-schema.js
@@ -4,7 +4,8 @@ const profileSchema = new mongoose.Schema({
     userid: String,
     uuid: String,
     userdata: String,
-    guilds: String
+    guilds: String,
+    VikkiVukAccountID: String
 })
 
 module.exports = mongoose.model('users', profileSchema)
