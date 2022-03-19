2faa833432fdffb39bace89d204388374bb4f54b
NisamVikki <viktorblazaric2@gmail.com>
2022-03-19 20:20:54 +0100

    Bot Update

--- web/routes/api/accounts.js
+++ web/routes/api/accounts.js
@@ -9,7 +9,6 @@ router.get("/link/redirect", passport.authenticate('discord'), (req, res, next) 
 })
 router.get("/link", async(req,res, next) => {
     let user = await req.user
-    console.log(user)
     if (user) {
         let params = await req.query
         if (params.uuid) {
@@ -17,7 +16,7 @@ router.get("/link", async(req,res, next) => {
                let response = await fetch("https://accounts.vikkivuk.xyz/getuser/" + params.uuid, {method: "GET",redirect:"follow"})
                let user = await response.json()
                if (user) {
-                   schema.findOneAndUpdate({userid: user.userid}, {VikkiVukAccountID:params.uuid})
+                   schema.findOneAndUpdate({userid: user.discordId}, {VikkiVukAccountID:params.uuid}, {new:true})
                    res.send("Your account has been linked! You can now safely return back to discord, and continue migrating your account.")
                }
            } catch(e) {
