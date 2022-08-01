5aaf94628138e1c601bf7c116e43a7359da3bb99
NisamVikki <viktorblazaric2@gmail.com>
2022-08-01 19:18:34 +0200

    Bot Update

--- bot/utility/advanced-polls.js
+++ bot/utility/advanced-polls.js
@@ -2,7 +2,7 @@ let channels = ["1001155291620053112", "1001156012297945108", "88953886060487479
 
 module.exports = (client) => {
     client.on('messageCreate', (msg) => {
-        if (channels.some(c => c === msg.channel.id)) {
+        if (channels.some(c => c == msg.channel.id)) {
             const { content } = msg
             const eachLine = content.split('\n')
 
